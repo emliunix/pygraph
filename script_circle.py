d5b8abdadabaf2395ff30090eb29998640e6709f
刘宇辉 <185127913@qq.com>
2016-06-14 15:13:52 +0800

    使用util中的saveG和enLarge替代自定义实现

--- script_circle.py
+++ script_circle.py
@@ -4,6 +4,7 @@ import numpy as np
 from pygraph.circle import mkCircle
 import matplotlib.pyplot as plt
 from pygraph.linestyle import *
+from pygraph.util import enLarge, saveG
 
 
 def job(fname, lstyle):
@@ -11,16 +12,9 @@ def job(fname, lstyle):
 
     mkCircle(g, (80, 60), 50, lstyle)
 
-    def enlarge(g, scale=10):
-        r = np.zeros(tuple(scale * x for x in g.shape))
-        for y in xrange(g.shape[0]):
-            for x in xrange(g.shape[1]):
-                r[y*scale:(y+1)*scale, x*scale:(x+1)*scale] = g[y, x]
-        return r
-
-    plt.imsave(fname, g, cmap="Greys_r")
+    saveG(fname, g)
     fname = "%s-large.%s" % (fname[0:fname.find(".")], fname[fname.find(".")+1:])
-    plt.imsave(fname, enlarge(g, 5), cmap="Greys_r")
+    saveG(fname, enLarge(g, 5))
 
 job("circle.png", None)
 job("circle-solid.png", SolidLineStyle())

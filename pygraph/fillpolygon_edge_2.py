57f610c61045a7aa1a672dffb0ca0ba1ca1314d8
刘宇辉 <185127913@qq.com>
2016-06-19 06:52:06 +0800

    使用 组对 的方法获取多边形填充中填充的范围

--- pygraph/fillpolygon_edge_2.py
+++ pygraph/fillpolygon_edge_2.py
@@ -40,10 +40,10 @@ def fillPolygonEdge2(g, *polygs):
     items = [expand(e) for e in edges]
 
     # determine boundary
-    lx = [p[0] for p in points]
+    # lx = [p[0] for p in points]
     ly = [p[1] for p in points]
-    x_max = max(lx)
-    x_min = min(lx)
+    # x_max = max(lx)
+    # x_min = min(lx)
     y_max = max(ly)
     y_min = min(ly)
 
@@ -52,17 +52,30 @@ def fillPolygonEdge2(g, *polygs):
 
         x_vals = [evalLineFun(y, i[0], i[4], i[5]) for i in items_in_range] # if x is met, filp _inside_
         x_vals.sort()
-        x_vals.reverse() # smaller x on the right, convenient to pop out
+        # x_vals.reverse() # smaller x on the right, convenient to pop out
 
-        inside = False
-        for x in xrange(x_min, x_max + 1):
-            if len(x_vals) and x_vals[-1] == x:
-                inside = not inside
-                tmp = x_vals.pop()
-                # current point is a lower point
-                if len(x_vals) and tmp == x_vals[-1]:
-                    x_vals.pop()
-                    inside = not inside
-                    g[y, x] = 1
-            if inside:
-                g[y, x] = 1
+        # pair up x_vals
+        i = 0
+        len_x_vals = len(x_vals)
+        x_pairs = []
+        # it must be an even value
+        assert len_x_vals % 2 == 0
+
+        while i < len_x_vals:
+            x_pairs.append((x_vals[i], x_vals[i+1]))
+            i += 2
+
+        # inside = False
+        # for x in xrange(x_min, x_max + 1):
+        #     if len(x_vals) and x_vals[-1] == x:
+        #         inside = not inside
+        #         tmp = x_vals.pop()
+        #         # current point is a lower point
+        #         if len(x_vals) and tmp == x_vals[-1]:
+        #             x_vals.pop()
+        #             inside = not inside
+        #             g[y, x] = 1
+        #     if inside:
+        #         g[y, x] = 1
+        for p in x_pairs:
+            g[y, p[0]:p[1]] = 1
